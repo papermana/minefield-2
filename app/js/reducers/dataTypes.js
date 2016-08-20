bb2f8473585c7b791733e3a7b9d35bf7ee9c0e95
yzuniversal <yzukonta@gmail.com>
2016-08-20 19:52:06 +0200

    Filled dataTypes.js with actual State classes

--- app/js/reducers/dataTypes.js
+++ app/js/reducers/dataTypes.js
@@ -1,10 +1,55 @@
 import Immutable from 'immutable';
+import createBoardLayout from '@utils/createBoardLayout';
 
 
-class State extends Immutable.Record({
-  foo: 'bar',
+class BoardConfig extends Immutable.Record({
+  rows: 10,
+  columns: 10,
+  mines: 12,
 }) {
   constructor(data) {
+    super(data);
+  }
+}
+
+class Status extends Immutable.Record({
+  time: 0,
+  flagsDeployed: 0,
+  minesFlagged: 0,
+}) {
+  constructor(data) {
+    super(data);
+  }
+}
+
+class State extends Immutable.Record({
+  boardConfig: new BoardConfig(),
+  status: new Status(),
+  boardLayout: createBoardLayout(new BoardConfig()),
+  playerActions: Immutable.List(),
+}) {
+  constructor(data) {
+    if (data) {
+      if (data.boardConfig) {
+        data.boardConfig = new BoardConfig(data.boardConfig);
+      }
+
+      if (data.status) {
+        data.status = new Status(data.status);
+      }
+
+      if (data.boardLayout) {
+        data.boardLayout = Immutable.List(data.boardLayout);
+      }
+      else {
+        data.boardLayout = createBoardLayout(data.boardConfig || new BoardConfig());
+      }
+
+      if (data.playerActions) {
+        data.playerActions = Immutable.List(data.playerActions);
+      }
+    }
+
     super(data);
   }
 }
@@ -12,4 +57,6 @@ class State extends Immutable.Record({
 
 export {
   State,
+  BoardConfig,
+  Status,
 };
