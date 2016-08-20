5362ee3cc4843ce17f3ab89fbb40fd9587aa7a01
yzuniversal <yzukonta@gmail.com>
2016-08-20 19:44:28 +0200

    Fixed some issues with the testing setup

--- jestPreprocessor.js
+++ jestPreprocessor.js
@@ -1,3 +1,14 @@
 const config = require('./webpack.config.js');
+const aliasPreprocessor = require('jest-alias-preprocessor')(config);
+const babel = require('babel-jest').createTransformer({
+  plugins: 'transform-es2015-modules-commonjs',
+});
 
-module.exports = require('jest-alias-preprocessor')(config);
+module.exports = {
+  process: (src, path) => {
+    src = babel.process(src, path);
+    src = aliasPreprocessor.process(src, path);
+
+    return src;
+  },
+};
