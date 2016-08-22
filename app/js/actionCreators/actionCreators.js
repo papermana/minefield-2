bd20e15de5c377a5ba1b9a5e930e4837d8e61cc5
yzuniversal <yzukonta@gmail.com>
2016-08-22 22:53:24 +0200

    Tweak: revised actionCreators.js

--- app/js/actionCreators/actionCreators.js
+++ app/js/actionCreators/actionCreators.js
@@ -4,33 +4,24 @@ import rightClickField from './rightClickField';
 import startTimer from './startTimer';
 
 
-const actionCreators = {};
-
-for (const type in types) {
-  const name = type.toLowerCase().replace(/_(\w)/, (match, s1) => s1.toUpperCase());
-
-  actionCreators[name] = data => ({
-    type,
-    data,
-  });
-}
-
-actionCreators.clickField = clickField;
-actionCreators.rightClickField = rightClickField;
-actionCreators.startTimer = startTimer;
-
-actionCreators.showTopbar = () => ({
+const showTopbar = () => ({
   type: types.SHOW_TOPBAR,
   data: undefined,
 });
 
-actionCreators.hideTopbar = () => ({
+const hideTopbar = () => ({
   type: types.HIDE_TOPBAR,
   data: undefined,
 });
 
 
-export default actionCreators;
+export default {
+  clickField,
+  rightClickField,
+  startTimer,
+  showTopbar,
+  hideTopbar,
+};
 export {
   types,
 };
