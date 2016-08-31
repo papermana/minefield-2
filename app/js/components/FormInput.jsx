72d88969a9e7ea29b0b3135cfb3a0f9dabb0ffc3
yzuniversal <yzukonta@gmail.com>
2016-08-31 21:23:28 +0200

    Tweak: put FormInput in its own file

---
+++ app/js/components/FormInput.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import TextButton from '@components/TextButton';
+
+
+class FormInput extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.value,
+    };
+
+    this.singleDecrease = this.singleDecrease.bind(this);
+    this.singleIncrease = this.singleIncrease.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onInputBlur = this.onInputBlur.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    this.refs.input.addEventListener('keydown', this.onKeyDown);
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.setState({
+      value: newProps.value,
+    });
+  }
+
+  componentWillUnmount() {
+    this.refs.input.removeEventListener('keydown', this.onKeyDown);
+  }
+
+  singleDecrease() {
+    this.props.callback(this.props.name, this.props.value - 1);
+  }
+
+  singleIncrease() {
+    this.props.callback(this.props.name, this.props.value + 1);
+  }
+
+  onInputChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  onInputBlur() {
+    this.props.callback(this.props.name, Number.parseInt(this.state.value, 10));
+    this.setState({
+      value: this.props.value,
+    });
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === 13) {
+      this.onInputBlur();
+      this.props.confirm();
+    }
+    else if (e.keyCode === 109 || e.keyCode === 189) {
+      this.singleDecrease();
+    }
+    else if (e.keyCode === 107 || e.keyCode === 187) {
+      this.singleIncrease();
+    }
+    else {
+      return;
+    }
+
+    e.preventDefault();
+  }
+
+  render() {
+    const propsToPass = Object.assign({}, this.props);
+
+    delete propsToPass.callback;
+    delete propsToPass.confirm;
+
+    return <div>
+      <TextButton
+        light
+        onClick={this.singleDecrease} >
+        -
+      </TextButton>
+      <input ref="input"
+        {...propsToPass}
+        style={styles.input}
+        value={this.state.value}
+        onChange={this.onInputChange}
+        onBlur={this.onInputBlur} />
+      <TextButton
+        light
+        onClick={this.singleIncrease} >
+        +
+      </TextButton>
+    </div>;
+  }
+}
+
+FormInput.propTypes = {
+  callback: React.PropTypes.func.isRequired,
+  confirm: React.PropTypes.func.isRequired,
+  name: React.PropTypes.string.isRequired,
+  value: React.PropTypes.number.isRequired,
+};
+
+const styles = {
+  input: {
+    width: '5em',
+    backgroundColor: 'transparent',
+    border: 'none',
+    borderBottom: '2px solid white',
+    outline: 'none',
+    color: 'white',
+    fontSize: 26,
+  },
+};
+
+
+export default FormInput;
--- app/js/components/NewGameDialogUi.jsx
+++ app/js/components/NewGameDialogUi.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Immutable from 'immutable';
 import TextButton from '@components/TextButton';
+import FormInput from '@components/FormInput';
 
 
 const configPresets = {
@@ -19,108 +20,6 @@ const configPresets = {
     columns: 32,
     mines: 128,
   },
-};
-
-class Input extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      value: this.props.value,
-    };
-
-    this.singleDecrease = this.singleDecrease.bind(this);
-    this.singleIncrease = this.singleIncrease.bind(this);
-    this.onInputChange = this.onInputChange.bind(this);
-    this.onInputBlur = this.onInputBlur.bind(this);
-    this.onKeyDown = this.onKeyDown.bind(this);
-  }
-
-  componentDidMount() {
-    this.refs.input.addEventListener('keydown', this.onKeyDown);
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      value: newProps.value,
-    });
-  }
-
-  componentWillUnmount() {
-    this.refs.input.removeEventListener('keydown', this.onKeyDown);
-  }
-
-  singleDecrease() {
-    this.props.callback(this.props.name, this.props.value - 1);
-  }
-
-  singleIncrease() {
-    this.props.callback(this.props.name, this.props.value + 1);
-  }
-
-  onInputChange(e) {
-    this.setState({
-      value: e.target.value,
-    });
-  }
-
-  onInputBlur() {
-    this.props.callback(this.props.name, Number.parseInt(this.state.value, 10));
-    this.setState({
-      value: this.props.value,
-    });
-  }
-
-  onKeyDown(e) {
-    if (e.keyCode === 13) {
-      this.onInputBlur();
-      this.props.confirm();
-    }
-    else if (e.keyCode === 109 || e.keyCode === 189) {
-      this.singleDecrease();
-    }
-    else if (e.keyCode === 107 || e.keyCode === 187) {
-      this.singleIncrease();
-    }
-    else {
-      return;
-    }
-
-    e.preventDefault();
-  }
-
-  render() {
-    const propsToPass = Object.assign({}, this.props);
-
-    delete propsToPass.callback;
-    delete propsToPass.confirm;
-
-    return <div>
-      <TextButton
-        light
-        onClick={this.singleDecrease} >
-        -
-      </TextButton>
-      <input ref="input"
-        {...propsToPass}
-        style={styles.input}
-        value={this.state.value}
-        onChange={this.onInputChange}
-        onBlur={this.onInputBlur} />
-      <TextButton
-        light
-        onClick={this.singleIncrease} >
-        +
-      </TextButton>
-    </div>;
-  }
-}
-
-Input.propTypes = {
-  callback: React.PropTypes.func.isRequired,
-  confirm: React.PropTypes.func.isRequired,
-  name: React.PropTypes.string.isRequired,
-  value: React.PropTypes.number.isRequired,
 };
 
 class NewGameDialogUi extends React.PureComponent {
@@ -197,7 +96,7 @@ class NewGameDialogUi extends React.PureComponent {
 
           <div style={styles.messageRow} >
             Rows:
-            <Input name="rows"
+            <FormInput name="rows"
               autoFocus
               tabIndex={10}
               value={this.state.rows}
@@ -206,7 +105,7 @@ class NewGameDialogUi extends React.PureComponent {
           </div>
           <div style={styles.messageRow} >
             Columns:
-            <Input name="columns"
+            <FormInput name="columns"
               tabIndex={20}
               value={this.state.columns}
               callback={this.changeConfig}
@@ -214,7 +113,7 @@ class NewGameDialogUi extends React.PureComponent {
           </div>
           <div style={styles.messageRow} >
             Mines:
-            <Input name="mines"
+            <FormInput name="mines"
               tabIndex={30}
               value={this.state.mines}
               callback={this.changeConfig}
@@ -282,15 +181,6 @@ const styles = {
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: '0.5em',
-  },
-  input: {
-    width: '5em',
-    backgroundColor: 'transparent',
-    border: 'none',
-    borderBottom: '2px solid white',
-    outline: 'none',
-    color: 'white',
-    fontSize: 26,
   },
   confirmWrapper: {
     display: 'flex',
